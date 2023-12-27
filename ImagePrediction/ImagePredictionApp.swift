b0d7292b1027cfbeaa0f1b3892d7d9005ec53e4f
Eric Jenkinson <eric.jenkinson@ehjconsultancy.com>
2023-12-27 14:11:16 -0600

    Initial Commit

---
+++ ImagePrediction/ImagePredictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePredictionApp.swift
+//  ImagePrediction
+//
+//  Created by Eric Jenkinson on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ImagePredictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
